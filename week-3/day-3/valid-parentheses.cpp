a522e9c1b71099c144460f3b706017ee4866ef1a
avnishrajput11 <24110ew013@glbitm.ac.in>
2026-06-17 12:02:39 +0530

    week-2-day-3

---
+++ week-3/day-3/valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(char i:s){
+            if (i=='('||i=='['||i=='{')
+                st.push(i);
+            else{
+                if(st.empty())return false;
+                char top=st.top();
+                st.pop();
+                if(i=='}'&&top!='{')return false;
+                if(i==')'&&top!='(')return false;
+                if(i==']'&&top!='[')return false;
+            }    
+        }
+        return st.empty();
+    }
+};
