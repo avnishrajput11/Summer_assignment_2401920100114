8048cd1654f81e3abadf8bab4476edfbe5a924ba
avnishrajput11 <24110ew013@glbitm.ac.in>
2026-06-18 14:40:15 +0530

    week-3 day4

---
+++ week-3/day-4/evalute-reverse-polish-notation.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int>st;
+        int n=tokens.size();
+        for(string c : tokens){
+            if(c=="+"){
+                int second=st.top();
+                st.pop();
+                int first=st.top();
+                st.pop();
+                st.push(first+second);
+            }
+            else if(c=="-"){
+                int second=st.top();
+                st.pop();
+                int first=st.top();
+                st.pop();
+                st.push(first-second);
+            }
+            else if(c=="*"){
+                int second=st.top();
+                st.pop();
+                int first=st.top();
+                st.pop();
+                st.push(first*second);
+            }
+            else if(c=="/"){
+                int second=st.top();
+                st.pop();
+                int first=st.top();
+                st.pop();
+                st.push(first/second);
+            }
+            else{
+                st.push(stoi(c));
+            }
+        }
+        return st.top();
+    }
+};
